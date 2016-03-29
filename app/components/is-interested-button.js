edfc6e2edabb9b44fc8d7e0a6956dac8c165676b
Nick Iaconis <niaconis@linkedin.com>
2016-03-29 15:38:39 -0700

    Fix CP key; needs to not contain commas Also, provide hover state for falsy "interested" button

--- app/components/is-interested-button.js
+++ app/components/is-interested-button.js
@@ -8,16 +8,22 @@ export default Ember.Component.extend({
 
   interests: storageFor('interests'),
 
+  // Need to remove commas from the key b/c of
+  // github.com/emberjs/ember.js/blob/v2.4.0/packages/ember-metal/lib/expand_properties.js#L48-L52
+  storageKey: Ember.computed('talkId', function() {
+    return this.get('talkId').replace(/,/g, '');
+  }),
+
   didReceiveAttrs() {
     this._super(...arguments);
-    this.checked = Ember.computed(`interests.${this.get('talkId')}`, function() {
-      return this.getWithDefault(`interests.${this.get('talkId')}`, false);
+    this.checked = Ember.computed(`interests.${this.get('storageKey')}`, function() {
+      return this.getWithDefault(`interests.${this.get('storageKey')}`, false);
     });
   },
 
   checked: null,
 
   click() {
-    this.toggleProperty(`interests.${this.get('talkId')}`);
+    this.toggleProperty(`interests.${this.get('storageKey')}`);
   }
 });
