ada53740d0604a43e02b93c5039e68aca5aeae5c
Edward Faulkner <ef@alum.mit.edu>
2016-03-29 04:34:40 -0400

    more animations

--- app/transitions.js
+++ app/transitions.js
@@ -15,4 +15,22 @@ export default function() {
     this.use('to-left'),
     this.reverse('to-right')
   );
+
+  this.transition(
+    this.fromRoute('day.index'),
+    this.toRoute('day.talk'),
+    this.use('explode', {
+      matchBy: 'data-photo-id',
+      use: 'fly-to'
+    }, {
+      use: 'to-up'
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-photo-id',
+      use: 'fly-to'
+    }, {
+      use: 'to-down'
+    })
+
+  );
 }
